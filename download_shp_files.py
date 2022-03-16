7ace2b6a8289a6358154201f449bc6840d15d53c
khyarmt <khyarmt@gmail.com>
2022-03-16 17:37:06 +0900

    update

---
+++ download_shp_files.py
@@ -0,0 +1,34 @@
+import datetime
+import urllib.request
+import zipfile
+import subprocess
+import os
+
+download_server = 'https://download.geofabrik.de'
+region = 'asia'
+country = 'japan'
+
+#sub_regions = ['hokkaido', 'tohoku', 'kanto', 'chubu', 'kansai', 'chugoku', 'shikoku', 'kyushu']
+sub_regions = ['shikoku']
+
+for sub_region in sub_regions:
+    # Make Download Folder
+    processing_date = datetime.date.today().strftime('%Y-%m-%d') # スクリプトを実行した日付でフォルダを作成し、この中にデータを格納していく
+    if not os.path.exists(processing_date):
+        os.makedirs(processing_date)
+
+    # Download Zip
+    zip_name = f'{sub_region}-latest-free.shp.zip'
+    download_folder = processing_date
+    url = f'{download_server}/{region}/{country}/{zip_name}'
+    urllib.request.urlretrieve(url, f'{download_folder}/{zip_name}')
+
+    # Unzip Data
+    with zipfile.ZipFile(f'{download_folder}/{zip_name}') as zf:
+        extension_list = ['cpg', 'dbf', 'prj', 'shp', 'shx']
+        for extension in extension_list:
+            zf.extract(f'gis_osm_roads_free_1.{extension}', f'{download_folder}')
+            os.rename(f'{download_folder}/gis_osm_roads_free_1.{extension}', f'{download_folder}/gis_osm_roads_free_1_{sub_region}.{extension}')
+
+    # Deliete Unnecessary Files
+    #os.remove(f'{download_folder}/{zip_name}')
---
+++ make_road_network_data.py
@@ -0,0 +1,11 @@
+import json
+
+def make_road_network_data(import_folder, sub_region):
+    geojson_open = open(f'{import_folder}/gis_osm_roads_free_1_{sub_region}.geojson', 'r')
+    geojson_load = json.load(geojson_open)
+
+    road_list = geojson_load['features']
+    print(road_list[0]['geometry']['coordinates'])
+
+if __name__ == '__main__':
+    make_road_network_data()
